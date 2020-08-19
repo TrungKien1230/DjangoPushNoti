be94f9b0c36564765a3afabe4a8047db42671cfa
Trung-Kien NGUYEN <trung-kien.nguyen.external@airbus.com>
2020-08-19 14:35:21 +0200

    second commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ng8a2e5\TrungKien\Projects\WX\Source_code\DjangoPush\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
